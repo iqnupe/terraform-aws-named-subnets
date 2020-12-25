9b5d56e45e2292f02faa9a978660e6cfb5929b92
Frank Martin <iqnupe@users.noreply.github.com>
2020-12-25 13:40:10 -0600

    Update private.tf
    Update label module to use tf .14.0-compatible version.

--- private.tf
+++ private.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "private_label" {
-  source = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
+  source = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.22.0"
 
   attributes = compact(concat(module.this.attributes, ["private"]))
 
